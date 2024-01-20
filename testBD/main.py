f2df6a91bc291eaabb0d7cb039621b6265366dc3
Pavan Kumar V <pkvstarscream@gmail.com>
2024-01-20 23:40:14 +0530

    login and register added

--- testBD/main.py
+++ testBD/main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+import atexit
+from flask import Flask, render_template, request
 import mysql.connector
 from flask_bootstrap import Bootstrap5
 
@@ -11,16 +12,19 @@ db_config = {
     'port': 8889,
     'user': 'root',
     'password': 'root',
-    'database': 'mysql'
 }
 
 # Create MySQL Connection
 mysql_connection = mysql.connector.connect(**db_config)
 cursor = mysql_connection.cursor()
 
+cursor.execute(f'CREATE DATABASE IF NOT EXISTS taskforge')
+cursor.execute(f'USE taskforge')
+
 # Create table
 cursor.execute('''
-    CREATE TABLE IF NOT EXISTS users (
+    CREATE TABLE IF NOT EXISTS users 
+    (
         UserID INT AUTO_INCREMENT PRIMARY KEY,
         Username VARCHAR(255) NOT NULL,
         Password VARCHAR(255) NOT NULL,
@@ -28,16 +32,51 @@ cursor.execute('''
         Email VARCHAR(255) NOT NULL,
         Role VARCHAR(255) NOT NULL
     ) 
-''')
-
+    ''')
 mysql_connection.commit()
-cursor.close()
-mysql_connection.close()
+
+
+# except Exception as e:
+#     print(f'Error: {e}')
+# finally:
+# Close cursor and connection
 
 
 @app.route('/')
+def login():
+    return render_template('reglog.html')
+
+
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        new_user_data = {
+            'email': request.form.get('email'),
+            'name': request.form.get('name'),
+            'username': request.form.get('username'),
+            'role': request.form.get('role'),
+            'password': request.form.get('password')
+        }
+        cursor.execute('''
+            INSERT INTO users (Username, Password, FullName, Email, Role)
+                VALUES (%(username)s, %(password)s, %(name)s, %(email)s, %(role)s)
+        ''', new_user_data)
+        mysql_connection.commit()
+        return render_template('index.html', username=request.form.get('username'))
+    return render_template('sign-up.html')
+
+
+@app.route('/home')
 def home():
     return render_template('index.html')
+
+
+def close_db_connection():
+    cursor.close()
+    mysql_connection.close()
+
+# Register the function to be called on exit
+atexit.register(close_db_connection)
 
 if __name__ == "__main__":
     app.run(debug=True)
